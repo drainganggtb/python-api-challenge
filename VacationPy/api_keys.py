5328b046823c89032ceafd599835d963dcf9a36b
Justin <gree2167@umn.edu>
2020-10-31 05:06:36 -0500

    finishing up VacationPy, to do: markers and delete key

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "878406ce3423a7f53e6e4e632f154683"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
 g_key = "AIzaSyAuQLcV7VJEMolYoEVym53T8m6B86UOKaI"
