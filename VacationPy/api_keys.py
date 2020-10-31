9db38d93232f405f7d37df6e5a136a547ab80392
Justin <gree2167@umn.edu>
2020-10-30 19:30:54 -0500

    finally have the data!

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "fe9e5d0cd2b4bc8853fea575dd26b0a1"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAuQLcV7VJEMolYoEVym53T8m6B86UOKaI"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "878406ce3423a7f53e6e4e632f154683"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAuQLcV7VJEMolYoEVym53T8m6B86UOKaI"
